41af5be41856b6e42bebcc59d2446a7a86336e9c
Crescenzo Garofalo <crescenzogarofalo@macbook-pro-di-crescenzo.local>
2017-10-18 20:48:23 +0200

    Initial Commit

---
+++ MedicalAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedicalAlarm
+//
+//  Created by Crescenzo Garofalo on 18/10/17.
+//  Copyright © 2017 Crescenzo Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
